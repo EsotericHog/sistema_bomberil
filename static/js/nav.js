959114150a38b1010f081f6c339170c0d1d853fc
EsotericHog <juan.castillo18@outlook.com>
2025-08-05 02:39:48 -0400

    Diseño responsivo del layout para laptops pequeñas y tablets grandes
    feat(global): Diseño responsivo del layout para laptops pequeñas y tablets grandes
    - Adaptar para resoluciones inferiores a 1300px y 1100px
    - Menú desplegable para resoluciones inferiores a 1100px
    - Modificar Header y Nav para todas las aplicaciones
    
    feat(global): Botones para mostrar/ocultar barra lateral de navegación
    - Botón de menú de hamburguesa para desplegar el nav (visible < 1100px)
    - Botón con equis (X) para ocultar el nav (visible < 1100px)
    - Si el usuario cambia la resolución de la pantalla con el nav desplegado, éste se restablece
    - Si el usuario cierra el nav con opciones desplegadas, éstas se restablecen
    
    fix(inventario): Corregir área de la cuadrícula del main para navegadores estrictos.

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,25 +1,79 @@
-//Manipulación dinámica de elementos del nav
-const nav = document.getElementById('barra_lateral');
+//MaNIPULACIÓN DINÁMICA DE ELEMENTOS DEL NAV
 
+// Barra lateral de navegación
+const nav = document.getElementById('barra_lateral');
+// Secciones de la barra lateral
+const SeccionesBarraLateral = document.querySelectorAll('.barra_lateral__seccion');
+// Flechas de despliegue de cada sección
+const FlechasSeccionesBarraLateral = document.querySelectorAll('.barra_lateral__flecha_modulo');
+
+// Botón Menú hamburguesa para activar/desactivar sidebar
+const BotonToggleNav = document.getElementById('BotonToggleNav');
+// Botón para cerrar sidebar
+const BotonCerrarNav = document.getElementById('BotonCerrarNav');
+// Resolución para activar el menú hamburguesa
+const mediaQuery = window.matchMedia('(min-width: 1000px)');
+
+
+
+
+// Si se hace click en la flecha de cada módulo, se despliegan sus opciones
 if(nav) {
-    nav.addEventListener('click', (e)=>{
-        /*if(e.target.classList.contains('barra_lateral__modulo')) {
-                e.target.parentElement.classList.toggle('scale_nav_options');
-                e.target.children[1].classList.toggle('rotate180');
-            }
-            else if(e.target.classList.contains('barra_lateral__modulo_nombre')) {
-                e.target.parentElement.parentElement.classList.toggle('scale_nav_options');
-                e.target.nextElementSibling.classList.toggle('rotate180');
-            }
-            else if(e.target.classList.contains('barra_lateral__flecha_modulo')) {
-                e.target.parentElement.parentElement.classList.toggle('scale_nav_options');
-                e.target.classList.toggle('rotate180');
-            }
-        */
-       // Si se hace click en la flecha de cada módulo, se despliegan sus opciones
+    nav.addEventListener('click', (e) => {
         if(e.target.classList.contains('barra_lateral__flecha_modulo')) {
             e.target.parentElement.parentElement.classList.toggle('scale_nav_options');
             e.target.classList.toggle('rotate180');
         }
         });
 }
+
+
+
+// Mostrar/ocultar la barra lateral de navegación en resolución móvil
+if (BotonToggleNav && nav) {
+    BotonToggleNav.addEventListener('click', (e) => {
+        nav.classList.toggle('barra_lateral-activada');
+        BotonCerrarNav.classList.toggle('barra_lateral__boton_cerrar-activado');
+
+        SeccionesBarraLateral.forEach(seccion => {
+            seccion.classList.remove('scale_nav_options');
+        });
+        FlechasSeccionesBarraLateral.forEach(flecha => {
+            flecha.classList.remove('rotate180');
+        })
+        
+    })
+}
+
+
+
+// Ocultar barra lateral de navegación en resolución móvil
+if (BotonCerrarNav && nav && SeccionesBarraLateral && FlechasSeccionesBarraLateral) {
+    BotonCerrarNav.addEventListener('click', (e) => {
+        nav.classList.toggle('barra_lateral-activada');
+        BotonCerrarNav.classList.remove('barra_lateral__boton_cerrar-activado');
+
+        SeccionesBarraLateral.forEach(seccion => {
+            seccion.classList.remove('scale_nav_options');
+        });
+        FlechasSeccionesBarraLateral.forEach(flecha => {
+            flecha.classList.remove('rotate180');
+        })
+
+
+    })
+}
+
+
+
+// Reestablecer elementos cuando las dimensiones de la pantalla aumentan
+function handleResolutionChange(e) {
+  if (e.matches) {
+    // Pantalla ancha - quitar clase
+    nav.classList.remove('barra_lateral-activada');
+    BotonCerrarNav.classList.remove('barra_lateral__boton_cerrar-activado');
+  }
+}
+mediaQuery.addEventListener('change', handleResolutionChange);
+// Ejecutar inmediatamente para el estado inicial
+handleResolutionChange(mediaQuery);
