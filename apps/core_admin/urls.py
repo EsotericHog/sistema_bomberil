c864c0094b0d1d3a759acd96a2fd077ca3642a10
Pelaoconbeatle <juan.castillo18@utlook.com>
2025-11-18 17:59:14 -0300

    feat(core_admin): implementar listado de estaciones
    - Agregar 'EstacionListView' con optimización de consultas (select_related).
    - Diseñar plantilla 'estacion_list.html' con manejo robusto de imágenes (fallback) y badges de estado.
    - Configurar URLs iniciales del módulo.

--- apps/core_admin/urls.py
+++ apps/core_admin/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import AdministracionInicioView
+from .views import AdministracionInicioView, EstacionListView
 
 app_name = 'core_admin'
 
 urlpatterns = [
     # Página Inicial
     path('', AdministracionInicioView.as_view(), name="ruta_inicio"),
+
+    # Lista de estaciones
+    path('estaciones/', EstacionListView.as_view(), name='ruta_lista_estaciones'),
 
 ]
--- apps/core_admin/views.py
+++ apps/core_admin/views.py
@@ -1,8 +1,41 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
 
-# Create your views here.
+from .mixins import SuperuserRequiredMixin
+from apps.gestion_inventario.models import Estacion
+
+
 class AdministracionInicioView(View):
     template_name = "core_admin/pages/home.html"
     def get(self, request):
         return render(request, self.template_name)
+    
+
+
+
+class EstacionListView(SuperuserRequiredMixin, ListView):
+    model = Estacion
+    template_name = 'core_admin/pages/lista_estaciones.html'
+    context_object_name = 'estaciones'
+    paginate_by = 10  # Paginación para no saturar la vista si crecen las compañías
+    
+    def get_queryset(self):
+        """
+        Retorna las estaciones optimizando la consulta a la DB.
+        Ordenamos por nombre por defecto.
+        """
+        queryset = Estacion.objects.select_related('comuna', 'comuna__region').all().order_by('nombre')
+        
+        # Opcional: Si quieres añadir un buscador simple por nombre
+        q = self.request.GET.get('q')
+        if q:
+            queryset = queryset.filter(nombre__icontains=q)
+            
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo_pagina'] = "Administración de Estaciones"
+        context['segmento'] = "estaciones" # Para resaltar el menú lateral si usas uno
+        return context
